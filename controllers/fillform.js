c322f419007661fd9d6520b65edaacffedb69f3a
dvainanidispro <vainanidis@computerstudio.gr>
2024-06-17 18:16:20 +0300

    0.7 - Ελληνικά πεδία κειμένου

--- controllers/fillform.js
+++ controllers/fillform.js
@@ -1,11 +1,17 @@
 'use strict'
 
-import { response } from 'express';
 import { PDFDocument } from 'pdf-lib';
+import fs from 'fs';
+
+import fontkit from '@pdf-lib/fontkit'
+// const fontLocation = './public/fonts/OpenSans-Regular.ttf';
+const fontLocation = './public/fonts/Ubuntu-R.ttf';     // ΠΡΟΣΟΧΗ! Να είναι path σχετικό με το κύριο script που τρέχει (server.js)
+const fontBytes = fs.readFileSync(fontLocation);
 
 
+ 
 /** Συμπληρώνει ένα συγκεκριμένο πεδίο της φόρμας με τη συγκεκριμένη τιμή */
-function fillField(form, fieldName, fieldValue){
+function fillField(form, fieldName, fieldValue, font){
 
     /** Το πεδίο του PDF */
     let field;
@@ -19,13 +25,14 @@ function fillField(form, fieldName, fieldValue){
     
     switch(fieldType){
         case 'PDFTextField':
-            form.getTextField(fieldName).setText(fieldValue);
+            form.getTextField(fieldName).setText(fieldValue, {font: font});
+            field.updateAppearances(font);
             break;
         case 'PDFCheckBox':
             form.getCheckBox(fieldName).check();
             break;
         case 'PDFRadioGroup':
-            form.getRadioGroup(fieldName).select(fieldValue);
+            form.getRadioGroup(fieldName).select(fieldValue, {font: font});
             break;
         default:
             break;
@@ -48,11 +55,15 @@ async function fillForm(pdfLocation, formData){
             return false;
         });
     if(!pdfBytes){ return false }
+    
     const pdfDoc = await PDFDocument.load(pdfBytes);
+    pdfDoc.registerFontkit(fontkit);
+    const unicodeFont = await pdfDoc.embedFont(fontBytes);
     const form = pdfDoc.getForm();
+
     // const fields = form.getFields();
     for (const [key, value] of Object.entries(formData)) {
-        fillField(form, key, value);
+        fillField(form, key, value, unicodeFont);
     }
     const pdfBytesFilled = await pdfDoc.save();
     return pdfBytesFilled;
